eb708ece7309142b9bfc48c3264b69277fb9dd34
Eduardo Semkiw Xavier <eduardosemkiwxavier@gmail.com>
2022-07-21 00:59:24 -0300

    Primeiro commit

---
+++ src/main/java/com/eduardosemkiwxavier/github_actions_springboot/GithubActionsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.eduardosemkiwxavier.github_actions_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsSpringbootApplication.class, args);
+	}
+
+}
